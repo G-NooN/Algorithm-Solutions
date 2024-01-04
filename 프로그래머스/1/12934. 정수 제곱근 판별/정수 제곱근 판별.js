13f52fd08cdf212eaffc6036a4bdf2696cbe8ebc
G-NooN Jo <68181274+G-NooN@users.noreply.github.com>
2024-01-04 19:55:16 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
@@ -1,5 +1,5 @@
 function solution(n) {
-    let answer = 0; // true/false
+    let answer = 0;
     
     // n이 "양의 정수"의 제곱수 (양의 정수 : % 1 === 0)
     answer = Math.sqrt(n) % 1 === 0 ? Math.pow(Math.sqrt(n) + 1, 2) : -1;
