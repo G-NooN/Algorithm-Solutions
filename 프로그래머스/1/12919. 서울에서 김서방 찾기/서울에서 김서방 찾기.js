399efe9cd49dd7bd1d98976f05d4a6d6f18ece6f
G-NooN Jo <68181274+G-NooN@users.noreply.github.com>
2024-01-08 12:37:34 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.js
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.js
@@ -1,14 +1,9 @@
 function solution(seoul) {
     let answer = '';
-    let kimIndex = 0;
     
-    for(let i=0; i<seoul.length; i++) {
-        if(seoul[i] === "Kim") {
-            kimIndex = i;
-        }
-    }
+    let x = seoul.indexOf("Kim");
     
-    answer = `김서방은 ${kimIndex}에 있다`
+    answer = `김서방은 ${x}에 있다`
     
     return answer;
 }
