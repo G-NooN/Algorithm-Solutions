fb65f2cf2dc45b204cf81cd65236da761a14189f
G-NooN Jo <68181274+G-NooN@users.noreply.github.com>
2024-01-10 00:23:37 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -1,16 +1,13 @@
 function solution(num) {
-    let answer = 0;
+  let answer = 0; 
+
+  while (answer < 500) {
+    if (num === 1) break;
+    else num % 2 === 0 ? (num /= 2) : (num = num * 3 + 1);
+    answer++;
+  }
     
-    while(answer < 500) {
-        if(num === 1) break;
-        else if(num % 2 === 0) num /= 2;
-        else {
-            num = num * 3 + 1;
-        }
-        answer++;
-    }
-    
-    if(num !== 1) answer = -1;
-    
-    return answer;
+  if (num !== 1) answer = -1;
+
+  return answer;
 }
