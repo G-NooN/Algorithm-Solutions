ed14884b2605de483aead9402fcc280ddf0c1d9b
G-NooN Jo <68181274+G-NooN@users.noreply.github.com>
2024-01-10 00:56:42 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -1,12 +1,8 @@
 function solution(phone_number) {
-    let answer = '';
-    let stars = '';
-    
-    for(let i=0; i<phone_number.length-4; i++) {
-        stars += '*';
-    }
-    
-    answer = stars.concat(phone_number.substring(phone_number.length - 4))
-    
-    return answer;
+  let answer = "";
+  let lastFour = phone_number.substring(phone_number.length - 4);
+
+  answer = "*".repeat(phone_number.length - 4).concat(lastFour);
+
+  return answer;
 }
