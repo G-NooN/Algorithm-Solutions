01739f64cb1b7dfdc83466941789152070dcc573
G-NooN Jo <68181274+G-NooN@users.noreply.github.com>
2024-01-15 09:13:55 +0900

    [level Lv.1] Title: 내적, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.1/70128. 내적/내적.js
@@ -0,0 +1,11 @@
+function solution(a, b) {
+    var answer = 1234567890;
+     let sum = 0;
+
+  for(let i=0; i<a.length; i++) {
+    sum += a[i]*b[i];
+  }
+
+  answer = sum;
+    return answer;
+}
