b5d20ebca00f452c02b8f8d334dc2f616798fc5e
G-NooN Jo <68181274+G-NooN@users.noreply.github.com>
2024-01-17 12:56:55 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
@@ -0,0 +1,11 @@
+function solution(s) {
+    let answer = true;
+    
+    if (s.length !== 4 && s.length !== 6) answer = false;
+
+    for (let i = 0; i < s.length; i++) {
+        if (isNaN(Number(s[i]))) answer = false;
+    }
+    
+    return answer;
+}
