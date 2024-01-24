0258aef822ca17a03758f64e8700397bdd5fe641
G-NooN Jo <68181274+G-NooN@users.noreply.github.com>
2024-01-24 09:36:18 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.js
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.js
@@ -1,7 +1,7 @@
 function solution(n) {
     let answer = 0;
     const ternaryN = n.toString(3);
-    const reverseTernaryN = String(ternaryN).split('').reverse().join('');
+    const reverseTernaryN = ternaryN.split('').reverse().join('');
     
     answer = parseInt(reverseTernaryN, 3);
     
