3cf0f94e912481d64a09e44b86c46d66a2718b34
G-NooN Jo <68181274+G-NooN@users.noreply.github.com>
2024-01-30 11:52:20 +0900

    [level 1] Title: 삼총사, Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.js
+++ 프로그래머스/1/131705. 삼총사/삼총사.js
@@ -2,9 +2,9 @@ function solution(number) {
   let answer = 0;
 
   for (let i = 0; i < number.length; i++) {
-    for (let j = 1; j < number.length; j++) {
-      for (let k = 1; k < number.length; k++) {
-        if (number[i] + number[i + j] + number[i + j + k] === 0) answer += 1;
+    for (let j = i + 1; j < number.length; j++) {
+      for (let k = j + 1; k < number.length; k++) {
+        if (number[i] + number[j] + number[k] === 0) answer++;
       }
     }
   }
