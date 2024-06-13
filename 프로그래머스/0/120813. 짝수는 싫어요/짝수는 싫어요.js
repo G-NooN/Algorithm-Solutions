b811204ab2317c8155914fd0835ec0743ed0c7f2
G-NooN Jo <68181274+G-NooN@users.noreply.github.com>
2024-06-13 12:46:20 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.js
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.js
@@ -1,10 +1,9 @@
 function solution(n) {
   let answer = [];
-  for (let i = 0; i <= n; i++) {
-    if (i % 2 == 0) continue;
-    else {
-      answer.push(i);
-    }
+
+  for (let i = 1; i <= n; i += 2) {
+    answer.push(i);
   }
+
   return answer;
 }
