46f9b04ae572bb6aeeba25ff8c831eceb886a530
G-NooN Jo <68181274+G-NooN@users.noreply.github.com>
2025-11-25 18:03:47 +0900

    [Bronze III] Title: 곱셈, Time: 92 ms, Memory: 9588 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.ts
@@ -0,0 +1,23 @@
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().trim();
+
+const array = input.split("\n");
+
+const num1 = array[0];
+const num2 = array[1];
+
+const solution = (a: string, b: string) => {
+  const bArray = b.split("");
+
+  const sum1 = Number(a) * Number(bArray[2]);
+  const sum2 = Number(a) * Number(bArray[1]);
+  const sum3 = Number(a) * Number(bArray[0]);
+  const result = Number(a) * Number(b);
+
+  console.log(sum1);
+  console.log(sum2);
+  console.log(sum3);
+  console.log(result);
+};
+
+solution(num1, num2);
