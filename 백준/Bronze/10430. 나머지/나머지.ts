081bc7b6bba0e4ccb0fe0a2bfb25139cdc2fe063
G-NooN Jo <68181274+G-NooN@users.noreply.github.com>
2025-11-25 17:54:45 +0900

    [Bronze V] Title: 나머지, Time: 92 ms, Memory: 9588 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.ts
@@ -0,0 +1,17 @@
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().trim();
+
+const array = input.split(" ");
+
+const a = Number(array[0]);
+const b = Number(array[1]);
+const c = Number(array[2]);
+
+const solution = (a: number, b: number, c: number) => {
+  console.log((a + b) % c);
+  console.log(((a % c) + (b % c)) % c);
+  console.log((a * b) % c);
+  console.log(((a % c) * (b % c)) % c);
+};
+
+solution(a, b, c);
