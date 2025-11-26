7f1da82a093ae148ee7eeafe60253c0e62972dee
G-NooN Jo <68181274+G-NooN@users.noreply.github.com>
2025-11-26 20:13:05 +0900

    [Bronze V] Title: 사분면 고르기, Time: 92 ms, Memory: 9332 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.ts
@@ -0,0 +1,17 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim();
+
+const array = input.split("\n");
+
+const x = array[0];
+const y = array[1];
+
+const solution = (x: number, y: number) => {
+  if (x > 0) {
+    console.log(y > 0 ? 1 : 4);
+  } else {
+    console.log(y > 0 ? 2 : 3);
+  }
+};
+
+solution(Number(x), Number(y));
