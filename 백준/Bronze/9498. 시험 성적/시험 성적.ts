18f95f258bb241fce39519b9f710d07ca05ff9a2
G-NooN Jo <68181274+G-NooN@users.noreply.github.com>
2025-11-26 19:18:58 +0900

    [Bronze V] Title: 시험 성적, Time: 92 ms, Memory: 9340 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.ts
@@ -0,0 +1,18 @@
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().trim();
+
+const solution = (score: number) => {
+  if (score >= 90) {
+    console.log("A");
+  } else if (score >= 80) {
+    console.log("B");
+  } else if (score >= 70) {
+    console.log("C");
+  } else if (score >= 60) {
+    console.log("D");
+  } else {
+    console.log("F");
+  }
+};
+
+solution(Number(input));
