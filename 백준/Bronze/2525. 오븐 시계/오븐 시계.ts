9c6de410d508bf0bee6c064ef6d04ca0fb218ddd
G-NooN Jo <68181274+G-NooN@users.noreply.github.com>
2025-11-27 14:19:30 +0900

    [Bronze III] Title: 오븐 시계, Time: 92 ms, Memory: 9336 KB -BaekjoonHub

---
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.ts
@@ -0,0 +1,28 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim();
+
+const array = input.split("\n");
+const timeArray = array[0].split(" ");
+
+const hour = timeArray[0];
+const minute = timeArray[1];
+const period = array[1];
+
+const solution = (hour: number, minute: number, period: number) => {
+  let targetHour = hour;
+  let targetMinute = minute + period;
+
+  while (targetMinute > 59) {
+    targetMinute -= 60;
+    targetHour += 1;
+  }
+
+  if (targetHour > 23) {
+    targetHour -= 24;
+  }
+
+  const result = `${targetHour} ${targetMinute}`;
+  console.log(result);
+};
+
+solution(Number(hour), Number(minute), Number(period));
