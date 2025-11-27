b395ed63ed173674c4da759634c8464407547953
G-NooN Jo <jjjchoon@nate.com>
2025-11-27 17:05:19 +0900

    feat: add b_25314 trial & error

---
+++ TrialAndError/b_25314.ts
@@ -0,0 +1,45 @@
+/**
+ * <코딩은 체육과목 입니다>
+ *
+ * - 레벨 : Bronze V
+ * - 링크 : https://www.acmicpc.net/problem/25314
+ */
+
+/**
+ * 입력형태 : 4
+ */
+
+// NOTE - 공통 상위 코드
+import fs from "fs";
+const input = fs.readFileSync("./baekjoon-test-input.txt").toString().trim();
+
+const value = Number(input);
+
+/**
+ * NOTE - 1st Trial (Success)
+ */
+
+const pastSolution = (value: number) => {
+  let longText = "";
+  const count = value / 4;
+
+  for (let i = 0; i < count; i++) {
+    longText += "long ";
+  }
+
+  console.log(longText + "int");
+};
+
+/**
+ * NOTE - 2nd(Final) Trial (Success)
+ * - 포인트
+ * : repeat
+ */
+const solution = (value: number) => {
+  const count = value / 4;
+  const longText = "long ";
+
+  console.log(`${longText.repeat(count)}int`);
+};
+
+solution(value);
