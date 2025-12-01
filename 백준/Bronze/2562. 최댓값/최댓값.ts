b0354e4397a29c686ee3b8496b92cbc629bc7195
G-NooN Jo <68181274+G-NooN@users.noreply.github.com>
2025-12-01 13:34:37 +0900

    [Bronze III] Title: 최댓값, Time: 88 ms, Memory: 9324 KB -BaekjoonHub

---
+++ 백준/Bronze/2562. 최댓값/최댓값.ts
@@ -0,0 +1,21 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim();
+
+const array = input.split("\n");
+
+let max = array[0];
+let maxIndex = 0;
+
+const solution = () => {
+  array.forEach((value, index) => {
+    if (Number(value) > Number(max)) {
+      max = value;
+      maxIndex = index;
+    }
+  });
+
+  console.log(max);
+  console.log(maxIndex + 1);
+};
+
+solution();
