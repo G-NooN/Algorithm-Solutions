3e8deef268b77df5f4789fa17634621272222709
G-NooN Jo <68181274+G-NooN@users.noreply.github.com>
2025-12-01 11:57:51 +0900

    [Bronze V] Title: A+B - 7, Time: 92 ms, Memory: 9516 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.ts
@@ -0,0 +1,17 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim();
+
+const array = input.split("\n");
+
+const [, ...values] = array;
+
+const solution = () => {
+  values.forEach((value, index) => {
+    const [a, b] = value.split(" ");
+    const sum = Number(a) + Number(b);
+
+    console.log(`Case #${index + 1}: ${sum}`);
+  });
+};
+
+solution();
