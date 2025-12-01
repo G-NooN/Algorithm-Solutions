0ddcc651fb29f47ebf27b084f9fb363b0941ad00
G-NooN Jo <jjjchoon@nate.com>
2025-12-01 12:55:10 +0900

    feat: add b_10871 trial & error

---
+++ TrialAndError/b_10871.ts
@@ -0,0 +1,44 @@
+/**
+ * <X보다 작은 수>
+ *
+ * - 레벨 : Bronze V
+ * - 링크 : https://www.acmicpc.net/problem/10871
+ */
+
+// NOTE - 공통 상위 코드
+import fs from "fs";
+const input = fs.readFileSync("./baekjoon-test-input.txt").toString().trim();
+
+const array = input.split("\n");
+const [targetArray, valueArray] = array;
+
+const [, target] = targetArray.split(" ");
+const values = valueArray.split(" ");
+
+/**
+ * NOTE - 1st Trial (Success)
+ */
+const pastSolution = () => {
+  let result = "";
+
+  values.forEach((value) => {
+    if (Number(value) < Number(target)) {
+      result += `${value} `;
+    }
+  });
+
+  console.log(result);
+};
+
+/**
+ * NOTE - 2nd Trial (Success)
+ * - 포인트
+ * : filter & join
+ */
+const solution = () => {
+  const targetValues = values.filter((value) => Number(value) < Number(target));
+
+  console.log(targetValues.join(" "));
+};
+
+solution();
