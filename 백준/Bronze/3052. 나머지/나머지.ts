94e9906dac4adb6d8e6821db23c9f699fc90ab63
G-NooN Jo <68181274+G-NooN@users.noreply.github.com>
2025-12-02 14:44:34 +0900

    [Bronze II] Title: 나머지, Time: 92 ms, Memory: 9332 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.ts
+++ 백준/Bronze/3052. 나머지/나머지.ts
@@ -4,19 +4,11 @@ const input = fs.readFileSync(0).toString().trim();
 const array = input
   .split("\n")
   .map(Number)
-  .map((value) => value % 42)
-  .sort((a, b) => a - b);
-
-let count = 1;
+  .map((value) => value % 42);
 
 const solution = () => {
-  for (let i = 0; i < array.length; i++) {
-    if (array[i] < array[i + 1]) {
-      count++;
-    }
-  }
-
-  console.log(count);
+  const targetArray = new Set(array);
+  console.log(targetArray.size);
 };
 
 solution();
