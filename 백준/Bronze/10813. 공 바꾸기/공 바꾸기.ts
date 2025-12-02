8b9c01a46e2c6a4b017ac466aa520df8649fca29
G-NooN Jo <68181274+G-NooN@users.noreply.github.com>
2025-12-02 14:00:02 +0900

    [Bronze II] Title: 공 바꾸기, Time: 104 ms, Memory: 9352 KB -BaekjoonHub

---
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.ts
@@ -0,0 +1,24 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim();
+
+const [targetArray, ...valuesArray] = input.split("\n");
+
+const [basketLength, _] = targetArray.split(" ").map(Number);
+
+const basketArray = Array.from({ length: basketLength }).map(
+  (_, index) => index + 1
+);
+
+const solution = () => {
+  valuesArray.forEach((value) => {
+    const [targetA, targetB] = value.split(" ").map(Number);
+    [basketArray[targetA - 1], basketArray[targetB - 1]] = [
+      basketArray[targetB - 1],
+      basketArray[targetA - 1],
+    ];
+  });
+
+  console.log(basketArray.join(" "));
+};
+
+solution();
