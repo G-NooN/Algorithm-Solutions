8c368d0d4426464f482a0fceaccdea03eedf36e2
G-NooN Jo <jjjchoon@nate.com>
2025-12-02 13:47:12 +0900

    feat: add b_10810 trial & error

--- TrialAndError/b_10810.ts
+++ TrialAndError/b_10810.ts
@@ -5,22 +5,23 @@
  * - 링크 : https://www.acmicpc.net/problem/10810
  */
 
+// NOTE - 공통 상위 코드
 import fs from "fs";
 const input = fs.readFileSync("./baekjoon-test-input.txt").toString().trim();
 
+const [targetArray, ...valuesArray] = input.split("\n");
+
+const [basketLength, chanceCount] = targetArray.split(" ").map(Number);
+
 /**
- * NOTE - Trial (Success, AI Assisted)
+ * NOTE - 1st Trial (Success, AI Assisted)
  * - 포인트
  * : 입력값 Number 처리 (map(Number))
  * : 결과 배열 생성
  * : 이중 반복문으로 각 반복마다 해당 index에 값 할당
  */
 
-const [targetArray, ...valuesArray] = input.split("\n");
-
-const [basketLength, chanceCount] = targetArray.split(" ").map(Number);
-
-const solution = () => {
+const pastSolution = () => {
   // 결과 배열
   const basketArray = Array.from({ length: basketLength }, () => 0);
 
@@ -36,6 +37,25 @@ const solution = () => {
       basketArray[j - 1] = number;
     }
   }
+
+  console.log(basketArray.join(" "));
+};
+
+/**
+ * NOTE - 2nd Trial (Success)
+ * - 포인트
+ * : forEach 메서드 사용
+ * : fill() 메서드 사용
+ * : 효과 - chanceCount 미사용, 코드 간소화
+ */
+
+const solution = () => {
+  const basketArray = Array.from({ length: basketLength }).fill(0);
+
+  valuesArray.forEach((value) => {
+    const [startIndex, endIndex, number] = value.split(" ").map(Number);
+    basketArray.fill(number, startIndex - 1, endIndex);
+  });
 
   console.log(basketArray.join(" "));
 };
