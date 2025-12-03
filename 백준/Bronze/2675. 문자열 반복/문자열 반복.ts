eb08c9be883ff395aa6a77bec3720f6fb4bccc54
G-NooN Jo <68181274+G-NooN@users.noreply.github.com>
2025-12-03 18:04:10 +0900

    [Bronze II] Title: 문자열 반복, Time: 92 ms, Memory: 9360 KB -BaekjoonHub

---
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.ts
@@ -0,0 +1,18 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim();
+
+const [, ...values] = input.split("\n");
+
+const solution = () => {
+  values.forEach((value) => {
+    const [count, letters] = value.split(" ");
+
+    const result = letters
+      .split("")
+      .map((letter) => letter.repeat(Number(count)));
+
+    console.log(result.join(""));
+  });
+};
+
+solution();
