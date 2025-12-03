0e6fdb81aeacff1bc5f7f1b6de09b7cff09c7781
G-NooN Jo <68181274+G-NooN@users.noreply.github.com>
2025-12-03 17:57:43 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 104 ms, Memory: 9344 KB -BaekjoonHub

---
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.ts
@@ -0,0 +1,16 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim();
+
+const letters = Array.from({ length: 26 }, (_, index) =>
+  String.fromCharCode(97 + index)
+);
+
+const solution = () => {
+  const result = letters.map((letter) =>
+    input.includes(letter) ? input.indexOf(letter) : -1
+  );
+
+  console.log(result.join(" "));
+};
+
+solution();
