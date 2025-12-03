273adc203b3468ffcfdb9a72e518807f45257137
G-NooN Jo <jjjchoon@nate.com>
2025-12-03 19:06:24 +0900

    feat: add b_1152 trial & error

---
+++ TrialAndError/b_1152.ts
@@ -0,0 +1,36 @@
+/**
+ * <단어의 개수>
+ *
+ * - 레벨 : Bronze II
+ * - 링크 : https://www.acmicpc.net/problem/1152
+ */
+
+// NOTE - 공통 상위 코드
+import fs from "fs";
+const input = fs.readFileSync("./baekjoon-test-input.txt").toString().trim();
+
+/**
+ * NOTE - 1st Trial (Fail)
+ * - 실패 원인
+ * : 문자열 분리 시 '' 도 하나의 단어로 카운트 됨
+ */
+
+const failedSolution = () => {
+  const words = input.trim().split(" ");
+  console.log(words.length);
+};
+
+/**
+ * NOTE - 2nd Trial (Success)
+ */
+
+const solution = () => {
+  const words = input
+    .trim()
+    .split(" ")
+    .filter((word) => word !== "");
+
+  console.log(words.length);
+};
+
+solution();
