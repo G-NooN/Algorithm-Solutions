8933b4e4d559985a61accf1f7418bf8ceb945092
G-NooN Jo <68181274+G-NooN@users.noreply.github.com>
2025-12-04 16:42:54 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 96 ms, Memory: 9336 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.ts
@@ -0,0 +1,13 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim();
+
+const array = input.split(" ").map(Number);
+
+const pieces = [1, 1, 2, 2, 2, 8];
+
+const solution = () => {
+  const result = array.map((value, index) => pieces[index] - value);
+  console.log(result.join(" "));
+};
+
+solution();
