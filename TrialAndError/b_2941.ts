a7b6ec4d6179d749b39d2f987cb549d91e4431b9
G-NooN Jo <jjjchoon@nate.com>
2025-12-05 15:42:22 +0900

    feat: add b_2941 trial & error

---
+++ TrialAndError/b_2941.ts
@@ -0,0 +1,31 @@
+/**
+ * <크로아티아 알파벳>
+ *
+ * - 레벨 : Silver V
+ * - 링크 : https://www.acmicpc.net/problem/2941
+ */
+
+// 공통 상위 코드
+import fs from "fs";
+const input = fs.readFileSync("./baekjoon-test-input.txt").toString().trim();
+
+/**
+ * NOTE - Trial (답지 참고)
+ * - 포인트
+ * : for..of 활용
+ * : replace() or replaceAll() 사용하여 텍스트 변환
+ */
+
+const croatianAlphabets = ["c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="];
+
+const solution = () => {
+  let replacedInput = input;
+
+  for (let alphabet of croatianAlphabets) {
+    replacedInput = replacedInput.replaceAll(alphabet, "@");
+  }
+
+  console.log(replacedInput.length);
+};
+
+solution();
