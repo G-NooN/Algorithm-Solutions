a75e24f17a7548f4723f924450e145d23de6d531
G-NooN Jo <jjjchoon@nate.com>
2025-12-07 16:27:34 +0900

    feat: add b_2738 trial & error

---
+++ TrialAndError/b_2738.ts
@@ -0,0 +1,45 @@
+/**
+ * <행렬 덧셈>
+ *
+ * - 레벨 : Bronze III
+ * - 링크 : https://www.acmicpc.net/problem/2738
+ */
+
+// NOTE - 공통 상위 코드
+import fs from "fs";
+const input = fs.readFileSync("./baekjoon-test-input.txt").toString().trim();
+
+/**
+ * NOTE - 1st Trial (Success)
+ * - 포인트
+ * : slice() 로 배열 분할
+ */
+
+const [size, ...values] = input.split("\n");
+
+const [row, column] = size.split(" ").map(Number);
+
+// 각 배열 -> 2차원 배열로 변환
+const array1 = values
+  .slice(0, row)
+  .map((value) => value.split(" ").map(Number));
+const array2 = values.slice(row).map((value) => value.split(" ").map(Number));
+
+// 결과 배열
+const resultArray = Array.from({ length: row }, () =>
+  Array.from({ length: column }, () => 0)
+);
+
+const solution = () => {
+  for (let i = 0; i < row; i++) {
+    for (let j = 0; j < column; j++) {
+      resultArray[i][j] = array1[i][j] + array2[i][j];
+    }
+  }
+
+  const result = resultArray.map((value) => value.join(" ")).join("\n");
+
+  console.log(result);
+};
+
+solution();
