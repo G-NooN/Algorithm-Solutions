295d3083b88c901f51657bc4edd7b94354aa8ca7
G-NooN Jo <68181274+G-NooN@users.noreply.github.com>
2025-12-10 01:19:23 +0900

    [Silver V] Title: 색종이, Time: 152 ms, Memory: 10068 KB -BaekjoonHub

---
+++ 백준/Silver/2563. 색종이/색종이.ts
@@ -0,0 +1,37 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim();
+
+const backgroundSize = 100;
+const paperSize = 10;
+
+const [, ...points] = input.split("\n");
+
+const background = Array.from({ length: backgroundSize }, () =>
+  Array.from({ length: backgroundSize }, () => 0)
+);
+
+const solution = () => {
+  points.forEach((point) => {
+    const [x, y] = point.split(" ").map(Number);
+
+    for (let i = x; i < x + paperSize; i++) {
+      for (let j = y; j < y + paperSize; j++) {
+        background[i][j]++;
+      }
+    }
+  });
+
+  let areaSize = 0;
+
+  for (let i = 0; i < backgroundSize; i++) {
+    for (let j = 0; j < backgroundSize; j++) {
+      if (background[i][j] > 0) {
+        areaSize++;
+      }
+    }
+  }
+
+  console.log(areaSize);
+};
+
+solution();
