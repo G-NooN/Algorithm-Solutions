cc65cdb629a8b7bc6005df3f64ccebd507ccb2af
G-NooN Jo <68181274+G-NooN@users.noreply.github.com>
2025-12-11 16:40:49 +0900

    [Bronze III] Title: 별 찍기 - 9, Time: 92 ms, Memory: 9436 KB -BaekjoonHub

--- 백준/Bronze/2446. 별 찍기 － 9/별 찍기 － 9.ts
+++ 백준/Bronze/2446. 별 찍기 － 9/별 찍기 － 9.ts
@@ -1,24 +1,18 @@
 import { readFileSync } from "fs";
-import { platform } from "process";
 
-const inputPath = platform === "linux" ? 0 : "./input.txt";
-const input = readFileSync(inputPath).toString().trim();
+const input = readFileSync(0).toString().trim();
 
-const count = Number(input);
+const max = Number(input);
+const starArray: string[] = [];
 
-function writeLine(i: number, desc?: boolean) {
-  const space = " ".repeat(i - 1);
-  const star = "*".repeat(
-    desc ? 2 * count - (2 * (i - 1) + 1) : 2 * (count - i) + 1
-  );
+for (let i = 0; i < max; i++) {
+  const tab = " ".repeat(i);
+  const star = "*".repeat(2 * (max - i) - 1);
 
-  console.log(space + star);
+  starArray.push(tab + star);
 }
 
-for (let i = 1; i < count; i++) {
-  writeLine(i, true);
-}
+const reversedArray = [...starArray].reverse().slice(1);
+const result = [...starArray, ...reversedArray];
 
-for (let i = count; i > 0; i--) {
-  writeLine(i);
-}
+console.log(result.join("\n"));
