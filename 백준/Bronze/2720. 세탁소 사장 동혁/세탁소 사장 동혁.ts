7b43c6e8c3df4455556d7bb3324d272f2a988afc
G-NooN Jo <68181274+G-NooN@users.noreply.github.com>
2025-12-13 02:06:59 +0900

    [Bronze III] Title: 세탁소 사장 동혁, Time: 84 ms, Memory: 9564 KB -BaekjoonHub

--- 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.ts
+++ 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.ts
@@ -4,21 +4,17 @@ const input = readFileSync(0).toString().trim();
 
 const [, ...values] = input.split("\n").map(Number);
 
-values.forEach((value) => {
-  let mod = 0;
+const changeList = [25, 10, 5, 1];
 
-  const qCount = Math.floor(value / 25);
+const resultArray = values.map((value) => {
+  const coinCountArray: number[] = [];
 
-  mod = value % 25;
+  changeList.forEach((change) => {
+    coinCountArray.push(Math.floor(value / change));
+    value %= change;
+  });
 
-  const dCount = Math.floor(mod / 10);
-
-  mod %= 10;
-
-  const nCount = Math.floor(mod / 5);
-  const pCount = mod % 5;
-
-  const resultText = `${qCount} ${dCount} ${nCount} ${pCount}`;
-
-  console.log(resultText);
+  return coinCountArray.join(" ");
 });
+
+console.log(resultArray.join("\n"));
