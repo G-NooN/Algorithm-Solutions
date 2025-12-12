21ae6981bc650cff1923e401e5bad883961f95d1
G-NooN Jo <68181274+G-NooN@users.noreply.github.com>
2025-12-12 16:36:54 +0900

    [Bronze III] Title: 별 찍기 - 20, Time: 104 ms, Memory: 9496 KB -BaekjoonHub

--- 백준/Bronze/10995. 별 찍기 － 20/별 찍기 － 20.ts
+++ 백준/Bronze/10995. 별 찍기 － 20/별 찍기 － 20.ts
@@ -4,10 +4,13 @@ const input = readFileSync(0).toString().trim();
 
 const count = Number(input);
 
+const starText = "* ".repeat(count);
+const reversedStarText = " *".repeat(count);
+
 for (let i = 1; i <= count; i++) {
   if (i % 2 === 0) {
-    console.log(" *".repeat(count));
+    console.log(reversedStarText);
   } else {
-    console.log("* ".repeat(count));
+    console.log(starText);
   }
 }
