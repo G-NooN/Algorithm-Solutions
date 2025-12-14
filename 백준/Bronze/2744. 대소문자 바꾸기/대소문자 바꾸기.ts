8bc3f02c09d341d2d5ab08448b4ad10ab7c2f608
G-NooN Jo <68181274+G-NooN@users.noreply.github.com>
2025-12-14 16:23:27 +0900

    [Bronze V] Title: 대소문자 바꾸기, Time: 92 ms, Memory: 9324 KB -BaekjoonHub

---
+++ 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.ts
@@ -0,0 +1,11 @@
+import { readFileSync } from "fs";
+
+const input = readFileSync(0).toString().trim();
+
+const array = input
+  .split("")
+  .map((letter) =>
+    letter.charCodeAt(0) > 96 ? letter.toUpperCase() : letter.toLowerCase()
+  );
+
+console.log(array.join(""));
