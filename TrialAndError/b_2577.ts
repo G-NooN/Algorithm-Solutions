34909dbf3df0602cf4e384c4773f4d2789648f48
G-NooN Jo <jjjchoon@nate.com>
2025-12-15 12:11:46 +0900

    feat: add b_2577 trial & error

---
+++ TrialAndError/b_2577.ts
@@ -0,0 +1,52 @@
+/**
+ * <숫자의 개수>
+ *
+ * - 레벨 : Bronze II
+ * - 링크 : https://www.acmicpc.net/problem/2577
+ */
+
+// NOTE - 공통 상위 코드
+import fs from "fs";
+const input = fs.readFileSync("./input.txt").toString().trim();
+
+const array = input.split("\n").map(Number);
+const multipleText = array
+  .reduce((acc, current) => acc * current)
+  .toString()
+  .split("");
+
+const numbers = Array.from({ length: 10 }, () => 0);
+
+/**
+ * NOTE - 1st Trial (Success)
+ * - 포인트
+ * : 숫자 0 ~ 9 를 배열 index로 활용
+ */
+
+const pastSolution = () => {
+  numbers.forEach((_, i) => {
+    multipleText.forEach((value) => {
+      if (value === i.toString()) {
+        numbers[i]++;
+      }
+    });
+  });
+
+  console.log(numbers.join("\n"));
+};
+
+/**
+ * NOTE - 2nd Trial (Success)
+ * - 포인트
+ * : 이중 반복문 제거 (index 자체가 숫자이기 때문에 바로 연결)
+ */
+
+const solution = () => {
+  multipleText.forEach((value) => {
+    numbers[Number(value)]++;
+  });
+
+  console.log(numbers.join("\n"));
+};
+
+solution();
