3478fc3c66c9e8ae7d9f7542a4aab750cebc4b77
G-NooN Jo <68181274+G-NooN@users.noreply.github.com>
2025-12-16 18:12:53 +0900

    [Silver V] Title: 분수찾기, Time: 96 ms, Memory: 9328 KB -BaekjoonHub

--- 백준/Silver/1193. 분수찾기/분수찾기.ts
+++ 백준/Silver/1193. 분수찾기/분수찾기.ts
@@ -2,18 +2,20 @@ import { readFileSync } from "fs";
 
 const input = readFileSync(0).toString().trim();
 
-let num = Number(input);
+const num = Number(input);
 
-let [crossCount, top, bottom] = [1, 1, 1];
+let [crossCount, currentNumber, top, bottom] = [1, 0, 1, 1];
 
-while (num > crossCount) {
-  num -= crossCount;
+while (num > crossCount + currentNumber) {
+  currentNumber += crossCount;
   crossCount++;
 }
 
+const position = num - currentNumber;
+
 const crossCountEven = crossCount % 2 === 0;
 
-top = crossCountEven ? num : crossCount - num + 1;
-bottom = crossCountEven ? crossCount - num + 1 : num;
+top = crossCountEven ? position : crossCount - position + 1;
+bottom = crossCountEven ? crossCount - position + 1 : position;
 
 console.log(`${top}/${bottom}`);
