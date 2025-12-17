1aac50272a4a3b125efc5f21ede188ff3a52dc7e
G-NooN Jo <68181274+G-NooN@users.noreply.github.com>
2025-12-17 19:02:07 +0900

    [Bronze III] Title: 웰컴 키트, Time: 92 ms, Memory: 9584 KB -BaekjoonHub

--- 백준/Bronze/30802. 웰컴 키트/웰컴 키트.ts
+++ 백준/Bronze/30802. 웰컴 키트/웰컴 키트.ts
@@ -4,17 +4,19 @@ const input = readFileSync(0).toString().trim();
 
 const [total, sizes, units] = input.split("\n");
 
+const count = Number(total);
 const [tUnit, pUnit] = units.split(" ").map(Number);
 
 const result: string[] = [];
 
 const tCount = sizes
   .split(" ")
-  .reduce((acc, size) => acc + Math.ceil(Number(size) / tUnit), 0);
+  .map(Number)
+  .reduce((acc, size) => acc + Math.ceil(size / tUnit), 0);
 
-const pCount = `${Math.floor(Number(total) / pUnit)} ${Number(total) % pUnit}`;
+const pCount = `${Math.floor(count / pUnit)} ${count % pUnit}`;
 
 result.push(String(tCount));
 result.push(pCount);
 
-console.log(result.join('\n'))
+console.log(result.join("\n"));
