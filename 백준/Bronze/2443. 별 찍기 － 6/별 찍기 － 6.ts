5f119b36004a50e17ef69781d9416d7dbafae9b8
G-NooN Jo <68181274+G-NooN@users.noreply.github.com>
2025-12-17 22:00:10 +0900

    [Bronze III] Title: 별 찍기 - 6, Time: 100 ms, Memory: 9540 KB -BaekjoonHub

--- 백준/Bronze/2443. 별 찍기 － 6/별 찍기 － 6.ts
+++ 백준/Bronze/2443. 별 찍기 － 6/별 찍기 － 6.ts
@@ -1,8 +1,6 @@
 import { readFileSync } from "fs";
-import { platform } from "process";
 
-const inputPath = platform === "linux" ? 0 : "./input.txt";
-const input = readFileSync(inputPath).toString().trim();
+const input = readFileSync(0).toString().trim();
 
 for (let i = Number(input); i > 0; i--) {
   const star = "*".repeat(2 * i - 1);
