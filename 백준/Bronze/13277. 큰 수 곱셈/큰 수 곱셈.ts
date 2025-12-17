675ac69f517fa703d50897936f63d125e159f854
G-NooN Jo <68181274+G-NooN@users.noreply.github.com>
2025-12-17 21:52:01 +0900

    [Bronze V] Title: 큰 수 곱셈, Time: 748 ms, Memory: 14340 KB -BaekjoonHub

--- 백준/Bronze/13277. 큰 수 곱셈/큰 수 곱셈.ts
+++ 백준/Bronze/13277. 큰 수 곱셈/큰 수 곱셈.ts
@@ -1,8 +1,6 @@
 import { readFileSync } from "fs";
-import { platform } from "process";
 
-const inputPath = platform === "linux" ? 0 : "./input.txt";
-const input = readFileSync(inputPath).toString().trim();
+const input = readFileSync(0).toString().trim();
 
 const [a, b] = input.split(" ").map(BigInt);
 
