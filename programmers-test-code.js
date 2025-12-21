3e28b15086964bd714640bef9c31fb69582a06f1
G-NooN Jo <jjjchoon@nate.com>
2025-12-21 17:06:01 +0900

    feat: update programmers test code
    - add function

--- programmers-test-code.js
+++ programmers-test-code.js
@@ -2,3 +2,5 @@ const { readFileSync } = require("fs");
 
 // const input = readFileSync(0).toString().trim();
 const input = readFileSync("./input.txt").toString().trim();
+
+function solution(input) {}
