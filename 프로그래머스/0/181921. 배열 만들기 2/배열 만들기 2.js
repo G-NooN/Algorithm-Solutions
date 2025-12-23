c0db65b5191e6af66881fbab5585e7d0caecac53
G-NooN Jo <68181274+G-NooN@users.noreply.github.com>
2025-12-24 02:43:16 +0900

    [level 0] Title: 배열 만들기 2, Time: 210.98 ms, Memory: 52.8 MB -BaekjoonHub

--- 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.js
+++ 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.js
@@ -2,16 +2,10 @@ function solution(l, r) {
   const result = [];
 
   for (let i = l; i <= r; i++) {
-    const [...value] = i.toString();
-
-    if (value.every((letter) => letter == "5" || letter == "0")) {
+    if (i.toString().replaceAll(/[05]/g, "").length === 0) {
       result.push(i);
     }
   }
 
-  if (!result.length) {
-    result.push(-1);
-  }
-
-  return result;
+  return result.length ? result : [-1];
 }
