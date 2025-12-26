cdd21299dd3c1a9f67e59676a16a234a407e0189
G-NooN Jo <68181274+G-NooN@users.noreply.github.com>
2025-12-26 14:15:09 +0900

    [level 0] Title: 배열 만들기 2, Time: 0.19 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.js
+++ 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.js
@@ -1,11 +1,9 @@
 function solution(l, r) {
   const result = [];
-
   let binaryNum = 1;
 
   while (true) {
-    const binaryNumText = binaryNum.toString(2);
-    const value = parseInt(binaryNumText, 10) * 5;
+    const value = binaryNum.toString(2) * 5;
 
     if (value > r) break;
     if (value >= l) result.push(value);
