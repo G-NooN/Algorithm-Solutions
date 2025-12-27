2b30c69cab68d00b210d5a8e4fc21016c0a58531
G-NooN Jo <68181274+G-NooN@users.noreply.github.com>
2025-12-27 20:26:44 +0900

    [level 0] Title: 배열 만들기 1, Time: 0.21 ms, Memory: 37 MB -BaekjoonHub

--- 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.js
+++ 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.js
@@ -1,5 +1,9 @@
 function solution(n, k) {
-  return Array.from({ length: n }, (_, index) => index + 1).filter(
-    (value) => value % k === 0
-  );
+  const result = [];
+
+  for (let i = k; i <= n; i += k) {
+    result.push(i);
+  }
+
+  return result;
 }
