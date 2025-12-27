485a088e82a7e7ec579a89fb3b21f88df065d9f7
G-NooN Jo <68181274+G-NooN@users.noreply.github.com>
2025-12-28 00:52:35 +0900

    [level 0] Title: 왼쪽 오른쪽, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.js
+++ 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.js
@@ -1,12 +1,9 @@
 function solution(str_list) {
-  const lIndex = str_list.findIndex((value) => value === "l");
-  const rIndex = str_list.findIndex((value) => value === "r");
+  const index = str_list.findIndex((value) => /l|r/.test(value));
 
-  if (lIndex < 0 && rIndex < 0) return [];
-  else if (lIndex >= 0 && rIndex >= 0) {
-    return lIndex < rIndex
-      ? str_list.slice(0, lIndex)
-      : str_list.slice(rIndex + 1);
-  } else
-    return lIndex >= 0 ? str_list.slice(0, lIndex) : str_list.slice(rIndex + 1);
+  if (index < 0) return [];
+
+  return str_list[index] === "l"
+    ? str_list.slice(0, index)
+    : str_list.slice(index + 1);
 }
