ef903db17b611604d84edc07c952db54579cd360
G-NooN Jo <68181274+G-NooN@users.noreply.github.com>
2025-12-28 01:41:03 +0900

    [Bronze V] Title: Bottle Return, Time: 92 ms, Memory: 9580 KB -BaekjoonHub

--- 백준/Bronze/21300. Bottle Return/Bottle Return.ts
+++ 백준/Bronze/21300. Bottle Return/Bottle Return.ts
@@ -2,9 +2,6 @@ import { readFileSync } from "fs";
 
 const input = readFileSync(0).toString().trim();
 
-const result = input
-  .split(" ")
-  .map((value) => Number(value) * 5)
-  .reduce((acc, value) => acc + value, 0);
+const result = input.split(" ").reduce((acc, value) => acc + Number(value), 0);
 
-console.log(result);
+console.log(result * 5);
