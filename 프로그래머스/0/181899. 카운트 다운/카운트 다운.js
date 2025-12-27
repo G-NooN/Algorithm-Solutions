608021301e7346035d29874ff0b437f23fdd02c0
G-NooN Jo <68181274+G-NooN@users.noreply.github.com>
2025-12-27 20:38:50 +0900

    [level 0] Title: 카운트 다운, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181899. 카운트 다운/카운트 다운.js
+++ 프로그래머스/0/181899. 카운트 다운/카운트 다운.js
@@ -1,9 +1,6 @@
 function solution(start_num, end_num) {
-  const result = [];
-
-  for (let i = start_num; i >= end_num; i--) {
-    result.push(i);
-  }
-
-  return result;
+  return Array.from(
+    { length: start_num - end_num + 1 },
+    (_, index) => start_num - index
+  );
 }
