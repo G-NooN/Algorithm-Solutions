4beb80f144f173b35bd749205e3f523c5da4875f
G-NooN Jo <68181274+G-NooN@users.noreply.github.com>
2025-12-27 20:21:18 +0900

    [level 0] Title: 문자 개수 세기, Time: 0.30 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.js
+++ 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.js
@@ -1,22 +1,11 @@
 function solution(my_string) {
-  const upperAlphabets = Array.from({ length: 26 }, () => 0);
-  const lowerAlphabets = Array.from({ length: 26 }, () => 0);
+  const alphabets = Array.from({ length: 52 }, (_, index) =>
+    String.fromCharCode(index < 26 ? 65 + index : 97 + index - 26)
+  ).join("");
 
-  [...my_string].forEach((letter) => {
-    const letterIndex = letter.charCodeAt(0);
-    const upperIndex = letterIndex - "A".charCodeAt(0);
-    const lowerIndex = letterIndex - "a".charCodeAt(0);
+  const result = Array.from({ length: 52 }, () => 0);
 
-    if (upperIndex < 26) {
-      upperAlphabets[upperIndex]++;
-    }
-
-    if (lowerIndex < 26) {
-      lowerAlphabets[lowerIndex]++;
-    }
-  });
-
-  const result = upperAlphabets.concat(lowerAlphabets);
+  [...my_string].forEach((letter) => result[alphabets.indexOf(letter)]++);
 
   return result;
 }
