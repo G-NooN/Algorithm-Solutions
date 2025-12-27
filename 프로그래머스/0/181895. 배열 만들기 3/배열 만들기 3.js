749735a8054d2c239b5201358cc7bb2a63e590bb
G-NooN Jo <68181274+G-NooN@users.noreply.github.com>
2025-12-27 21:22:21 +0900

    [level 0] Title: 배열 만들기 3, Time: 10.88 ms, Memory: 42.5 MB -BaekjoonHub

--- 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.js
+++ 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.js
@@ -1,11 +1,3 @@
 function solution(arr, intervals) {
-  let result = [];
-
-  intervals.forEach((value) => {
-    const [start, end] = value;
-
-    result = [...result, ...arr.slice(start, end + 1)];
-  });
-
-  return result;
+  return intervals.flatMap(([start, end]) => [...arr.slice(start, end + 1)]);
 }
