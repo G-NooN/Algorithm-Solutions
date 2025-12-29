4a5527879b213037c03642815e748dcf386a4e77
G-NooN Jo <68181274+G-NooN@users.noreply.github.com>
2025-12-29 11:33:26 +0900

    [level 0] Title: A 강조하기, Time: 0.04 ms, Memory: 33.1 MB -BaekjoonHub

--- 프로그래머스/0/181874. A 강조하기/A 강조하기.js
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.js
@@ -1,13 +1,3 @@
 function solution(myString) {
-  return [...myString]
-    .map((value) => {
-      switch (value) {
-        case "a":
-        case "A":
-          return "A";
-        default:
-          return value.toLowerCase();
-      }
-    })
-    .join("");
+  return myString.toLowerCase().replaceAll("a", "A");
 }
