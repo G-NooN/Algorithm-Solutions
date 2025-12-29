3270c468706eb81ded8638b93a6b3fbe13567d2e
G-NooN Jo <68181274+G-NooN@users.noreply.github.com>
2025-12-29 10:36:40 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 2, Time: 165.64 ms, Memory: 95.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.js
@@ -0,0 +1,27 @@
+function getMappedArray(array) {
+  return array.map((value) => {
+    const over50 = value >= 50;
+    const even = value % 2 === 0;
+
+    if (over50 && even) {
+      return value / 2;
+    }
+    if (!over50 && !even) {
+      return value * 2 + 1;
+    }
+    return value;
+  });
+}
+
+function solution(arr) {
+  let count = 0;
+
+  while (true) {
+    let mappedArray = getMappedArray(arr);
+
+    if (!mappedArray.every((value, index) => value === arr[index])) {
+      arr = mappedArray;
+      count++;
+    } else return count;
+  }
+}
