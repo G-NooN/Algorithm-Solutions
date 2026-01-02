0a1de1128e96f63940853e424172b1e584e2db8a
G-NooN Jo <68181274+G-NooN@users.noreply.github.com>
2026-01-02 13:11:47 +0900

    [level 0] Title: 세 개의 구분자, Time: 13.86 ms, Memory: 61.4 MB -BaekjoonHub

--- 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.js
+++ 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.js
@@ -1,8 +1,3 @@
 function solution(myStr) {
-  const result = myStr
-    .split("a")
-    .flatMap((value) => value.split("b").flatMap((value) => value.split("c")))
-    .filter((value) => value.length);
-
-  return result.length ? result : ["EMPTY"];
+  return myStr.match(/[^a-c]+/g) ?? ["EMPTY"];
 }
