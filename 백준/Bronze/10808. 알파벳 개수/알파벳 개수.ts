86ef1a565e917f787e83eb791a815addde24e240
G-NooN Jo <68181274+G-NooN@users.noreply.github.com>
2026-01-04 22:24:53 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 100 ms, Memory: 9348 KB -BaekjoonHub

---
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.ts
@@ -0,0 +1,13 @@
+const fs = require('fs')
+
+const input = fs.readFileSync(0).toString().trim();
+
+const alphabets = Array.from({ length: 26 }, (_, index) =>
+  String.fromCharCode(97 + index)
+).join("");
+
+const result = Array.from({ length: 26 }, () => 0);
+
+[...input].forEach((letter) => result[alphabets.indexOf(letter)]++);
+
+console.log(result.join(" "));
