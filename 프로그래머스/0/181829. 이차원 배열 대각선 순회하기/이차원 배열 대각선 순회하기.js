e37ea32ecec30bd82cb1297076647daae37b7a4a
G-NooN Jo <68181274+G-NooN@users.noreply.github.com>
2026-01-06 17:55:10 +0900

    [level 0] Title: 이차원 배열 대각선 순회하기, Time: 0.22 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181829. 이차원 배열 대각선 순회하기/이차원 배열 대각선 순회하기.js
+++ 프로그래머스/0/181829. 이차원 배열 대각선 순회하기/이차원 배열 대각선 순회하기.js
@@ -2,8 +2,7 @@ function solution(board, k) {
   let total = 0;
 
   for (let i = 0; i < board.length; i++) {
-    for (let j = 0; j < board[0].length; j++) {
-      if (i + j > k) continue;
+    for (let j = 0; i + j <= k && j < board[0].length; j++) {
       total += board[i][j];
     }
   }
