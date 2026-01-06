cac31c58f5a5e03bfd2d949d3a3eb8051824ca9e
G-NooN Jo <68181274+G-NooN@users.noreply.github.com>
2026-01-06 17:03:58 +0900

    [level 0] Title: l로 만들기, Time: 6.20 ms, Memory: 36.2 MB -BaekjoonHub

--- 프로그래머스/0/181834. l로 만들기/l로 만들기.js
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.js
@@ -1,5 +1,3 @@
 function solution(myString) {
-  return [...myString]
-    .map((value) => ("l".charCodeAt(0) - value.charCodeAt(0) > 0 ? "l" : value))
-    .join("");
+  return myString.replaceAll(/[a-k]/g, "l");
 }
