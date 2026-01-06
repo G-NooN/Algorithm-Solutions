8609dee59d822f02fa41dd35e828537dac89c7bc
G-NooN Jo <jjjchoon@nate.com>
2026-01-06 20:41:22 +0900

    chore: delete programmers legacy code

--- 프로그래머스/unrated/120807. 숫자 비교하기/숫자 비교하기.js
+++
@@ -1,5 +0,0 @@
-function solution(num1, num2) {
-    let answer = (num1 === num2) ? 1 : -1;
-    
-    return answer;
-}
--- 프로그래머스/unrated/120820. 나이 출력/나이 출력.js
+++
@@ -1,9 +0,0 @@
-function solution(age) {
-    let answer = 0;
-    
-    // answer = new Date().getFullYear() - age + 1;
-    
-    answer = 2022 - age + 1;
-    
-    return answer;
-}
