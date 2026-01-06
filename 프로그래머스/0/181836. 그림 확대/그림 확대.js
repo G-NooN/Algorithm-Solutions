d0dbab7a0f38b958d08400319ca28b3c4103e680
G-NooN Jo <68181274+G-NooN@users.noreply.github.com>
2026-01-06 16:54:36 +0900

    [level 0] Title: 그림 확대, Time: 0.26 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/181836. 그림 확대/그림 확대.js
+++ 프로그래머스/0/181836. 그림 확대/그림 확대.js
@@ -2,9 +2,10 @@ function solution(picture, k) {
   const result = [];
 
   picture.forEach((value) => {
+    const piece = [...value].reduce((acc, curr) => acc + curr.repeat(k), "");
+
     for (let i = 0; i < k; i++) {
-      const piece = [...value].map((letter) => letter.repeat(k));
-      result.push(piece.join(""));
+      result.push(piece);
     }
   });
 
