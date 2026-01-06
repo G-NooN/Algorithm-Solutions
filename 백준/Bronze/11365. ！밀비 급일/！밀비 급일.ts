c436d6a9f41887303c58b2996cd03157a56b0345
G-NooN Jo <68181274+G-NooN@users.noreply.github.com>
2026-01-06 21:05:41 +0900

    [Bronze IV] Title: !밀비 급일, Time: 88 ms, Memory: 9344 KB -BaekjoonHub

---
+++ 백준/Bronze/11365. ！밀비 급일/！밀비 급일.ts
@@ -0,0 +1,10 @@
+const fs = require("fs");
+
+const input = fs.readFileSync(0).toString().trim();
+
+const array = input
+  .split("\n")
+  .slice(0, -1)
+  .map((value) => [...value].reverse().join(""));
+
+console.log(array.join("\n"));
