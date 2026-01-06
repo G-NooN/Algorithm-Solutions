0bf66bad220cb0ddb92a31d0b659851b707c21ff
G-NooN Jo <68181274+G-NooN@users.noreply.github.com>
2026-01-06 18:07:54 +0900

    [level 0] Title: 정사각형으로 만들기, Time: 2.04 ms, Memory: 38.2 MB -BaekjoonHub

--- 프로그래머스/0/181830. 정사각형으로 만들기/정사각형으로 만들기.js
+++ 프로그래머스/0/181830. 정사각형으로 만들기/정사각형으로 만들기.js
@@ -1,16 +1,17 @@
 function solution(arr) {
   const valueLength = arr[0].length;
+  const diff = Math.abs(arr.length - valueLength);
 
   if (valueLength > arr.length) {
     return arr.concat(
-      Array.from({ length: valueLength - arr.length }, () =>
+      Array.from({ length: diff }, () =>
         Array.from({ length: valueLength }, () => 0)
       )
     );
   } else if (valueLength < arr.length) {
     return arr.map((value) => [
       ...value,
-      ...Array.from({ length: arr.length - valueLength }, () => 0),
+      ...Array.from({ length: diff }, () => 0),
     ]);
   } else return arr;
 }
