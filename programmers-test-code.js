1a4b0b4b366ea8937608b10af26ae0bc1f8ce01f
G-NooN Jo <jjjchoon@nate.com>
2026-01-09 09:38:28 +0900

    feat: add programmers test code local file path

--- programmers-test-code.js
+++ programmers-test-code.js
@@ -1,6 +1,7 @@
 // const { readFileSync } = require("fs");
 
 // const input = readFileSync(0).toString().trim();
+// const input = readFileSync("./input.txt").toString().trim();
 
 function solution() {}
 
