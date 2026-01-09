d4c79044495f1d986ea4412d653a115f22fabe5f
G-NooN Jo <68181274+G-NooN@users.noreply.github.com>
2026-01-09 10:02:46 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
@@ -1,14 +1,7 @@
 function solution(num_list) {
-  const result = num_list.reduce(
-    (acc, value) => {
-      let [evenCount, oddCount] = acc;
+  let result = [0, 0];
 
-      value % 2 === 0 ? evenCount++ : oddCount++;
-
-      return [evenCount, oddCount];
-    },
-    [0, 0]
-  );
+  num_list.forEach((value) => result[value % 2]++);
 
   return result;
 }
