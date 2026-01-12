4042c9f2c3eb63214deee92a8d9648ebf4d2056c
G-NooN Jo <68181274+G-NooN@users.noreply.github.com>
2026-01-12 22:44:22 +0900

    [Bronze IV] Title: 타임 카드, Time: 88 ms, Memory: 9324 KB -BaekjoonHub

--- 백준/Bronze/5575. 타임 카드/타임 카드.ts
+++ 백준/Bronze/5575. 타임 카드/타임 카드.ts
@@ -2,19 +2,20 @@ import { readFileSync } from "fs";
 
 const input = readFileSync(0).toString().trim();
 
-const array = input.split("\n").map((value) => {
+const result = input.split("\n").map((value) => {
   const [startHour, startMinute, startSecond, endHour, endMinute, endSecond] =
     value.split(" ").map(Number);
 
-  const targetSecond = endSecond - startSecond;
-  const resultSecond = targetSecond < 0 ? 60 + targetSecond : targetSecond;
+  const startTime = startHour * 3600 + startMinute * 60 + startSecond;
+  const endTime = endHour * 3600 + endMinute * 60 + endSecond;
 
-  const targetMinute = endMinute - startMinute - (targetSecond < 0 ? 1 : 0);
-  const resultMinute = targetMinute < 0 ? 60 + targetMinute : targetMinute;
+  const resultTime = endTime - startTime;
 
-  const resultHour = endHour - startHour - (targetMinute < 0 ? 1 : 0);
+  const resultHour = Math.floor(resultTime / 3600);
+  const resultMinute = Math.floor((resultTime % 3600) / 60);
+  const resultSecond = resultTime % 60;
 
   return `${resultHour} ${resultMinute} ${resultSecond}`;
 });
 
-console.log(array.join("\n"));
+console.log(result.join("\n"));
