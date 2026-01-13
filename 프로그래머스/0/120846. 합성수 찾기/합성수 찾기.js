f659517e1e2d2c30f947718d42fbb0db1c2f7eec
G-NooN Jo <68181274+G-NooN@users.noreply.github.com>
2026-01-13 18:22:14 +0900

    [level 0] Title: 합성수 찾기, Time: 0.20 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.js
@@ -0,0 +1,19 @@
+function solution(n) {
+  let result = 0;
+
+  for (let i = 4; i <= n; i++) {
+    let count = 0;
+
+    for (let j = 1; j <= i; j++) {
+      if (i % j === 0) {
+        count++;
+      }
+    }
+
+    if (count > 2) {
+      result++;
+    }
+  }
+
+  return result;
+}
