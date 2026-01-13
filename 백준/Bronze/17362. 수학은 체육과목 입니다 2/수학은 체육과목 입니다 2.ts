f486ec9ce900ffb246770af4da130256fa6d8aa3
G-NooN Jo <68181274+G-NooN@users.noreply.github.com>
2026-01-13 20:32:41 +0900

    [Bronze IV] Title: 수학은 체육과목 입니다 2, Time: 92 ms, Memory: 9328 KB -BaekjoonHub

--- 백준/Bronze/17362. 수학은 체육과목 입니다 2/수학은 체육과목 입니다 2.ts
+++ 백준/Bronze/17362. 수학은 체육과목 입니다 2/수학은 체육과목 입니다 2.ts
@@ -2,21 +2,6 @@ import { readFileSync } from "fs";
 
 const input = readFileSync(0).toString().trim();
 
-const target = Math.floor((Number(input) - 1) / 4);
+const cycle = "12345432";
 
-const position = Number(input) % 4;
-
-switch (position) {
-  case 0:
-    console.log(target % 2 === 0 ? 4 : 2);
-    break;
-  case 1:
-    console.log(target % 2 === 0 ? 1 : 5);
-    break;
-  case 2:
-    console.log(target % 2 === 0 ? 2 : 4);
-    break;
-  case 3:
-    console.log(3);
-    break;
-}
+console.log(cycle[(Number(input) - 1) % 8]);
