7eac62900e04957e765e6c13d8162d8c9bdb7914
G-NooN Jo <68181274+G-NooN@users.noreply.github.com>
2026-01-14 16:30:53 +0900

    [level 0] Title: 컨트롤 제트, Time: 2.15 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.js
+++ 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.js
@@ -2,9 +2,9 @@ function solution(s) {
   const array = s.split(" ");
   const result = [];
 
-  for (let i = 0; i < array.length; i++) {
-    array[i] === "Z" ? result.pop() : result.push(array[i]);
-  }
+  array.forEach((value) => {
+    value === "Z" ? result.pop() : result.push(value);
+  });
 
   return result.reduce((acc, value) => acc + Number(value), 0);
 }
