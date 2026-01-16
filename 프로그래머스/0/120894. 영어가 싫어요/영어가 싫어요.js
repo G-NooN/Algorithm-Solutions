c52b9fa64eac0aaa63c6e2a6c713d5962b71ee68
G-NooN Jo <68181274+G-NooN@users.noreply.github.com>
2026-01-16 09:53:38 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.js
+++ 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.js
@@ -1,19 +1,21 @@
-const numberMap = {
-  one: 1,
-  two: 2,
-  three: 3,
-  four: 4,
-  five: 5,
-  six: 6,
-  seven: 7,
-  eight: 8,
-  nine: 9,
-  zero: 0,
-};
+const numberArray = [
+  "zero",
+  "one",
+  "two",
+  "three",
+  "four",
+  "five",
+  "six",
+  "seven",
+  "eight",
+  "nine",
+];
 
 function solution(numbers) {
-  const regex = /one|two|three|four|five|six|seven|eight|nine|zero/g;
-  const result = numbers.replace(regex, (match) => numberMap[match]);
+  const result = numberArray.reduce(
+    (acc, letter, index) => acc.replaceAll(letter, index),
+    numbers
+  );
 
   return Number(result);
 }
