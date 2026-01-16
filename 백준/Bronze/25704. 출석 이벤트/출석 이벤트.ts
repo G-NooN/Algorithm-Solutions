e1332941581cfe10a7c356d0b4122ab44c7d2aa1
G-NooN Jo <68181274+G-NooN@users.noreply.github.com>
2026-01-16 12:16:32 +0900

    [Bronze IV] Title: 출석 이벤트, Time: 108 ms, Memory: 9608 KB -BaekjoonHub

--- 백준/Bronze/25704. 출석 이벤트/출석 이벤트.ts
+++ 백준/Bronze/25704. 출석 이벤트/출석 이벤트.ts
@@ -4,29 +4,12 @@ const input = readFileSync(0).toString().trim();
 
 const [count, price] = input.split("\n").map(Number);
 
-const [discount500, discount10, discount2000, discount25] = [
-  price - 500,
-  price * 0.9,
-  price - 2000,
-  price * 0.75,
-];
+const valueDiscountedPrice =
+  price - (count >= 15 ? 2000 : count >= 5 ? 500 : 0);
 
-let lowestPrice = price;
+const percentDiscountedPrice =
+  price * (count >= 20 ? 0.75 : count >= 10 ? 0.9 : 1);
 
-if (count >= 20) {
-  lowestPrice = Math.min(
-    lowestPrice,
-    discount500,
-    discount10,
-    discount2000,
-    discount25
-  );
-} else if (count >= 15) {
-  lowestPrice = Math.min(lowestPrice, discount500, discount10, discount2000);
-} else if (count >= 10) {
-  lowestPrice = Math.min(lowestPrice, discount500, discount10);
-} else if (count >= 5) {
-  lowestPrice = Math.min(lowestPrice, discount500);
-}
+const lowestPrice = Math.min(valueDiscountedPrice, percentDiscountedPrice);
 
-console.log(lowestPrice > 0 ? lowestPrice : 0);
+console.log(Math.max(lowestPrice, 0));
