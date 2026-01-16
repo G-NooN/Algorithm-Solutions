681c3ec29bacf0bbf5e464b42caa1de7bb7c76a8
G-NooN Jo <68181274+G-NooN@users.noreply.github.com>
2026-01-16 09:45:57 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
@@ -1,9 +1,7 @@
 function solution(my_string, num1, num2) {
-  return (
-    my_string.slice(0, num1) +
-    my_string[num2] +
-    my_string.slice(num1 + 1, num2) +
-    my_string[num1] +
-    my_string.slice(num2 + 1)
-  );
+  const result = [...my_string];
+
+  [result[num1], result[num2]] = [result[num2], result[num1]];
+
+  return result.join("");
 }
