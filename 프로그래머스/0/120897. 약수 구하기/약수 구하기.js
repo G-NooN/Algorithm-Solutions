ed8cedfbbe0ca9c2fa01499421f45940a3ffe339
G-NooN Jo <68181274+G-NooN@users.noreply.github.com>
2026-01-16 09:31:41 +0900

    [level 0] Title: 약수 구하기, Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120897. 약수 구하기/약수 구하기.js
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.js
@@ -1,7 +1,7 @@
 function solution(n) {
-  const result = new Set([1, n]);
+  const result = new Set();
 
-  for (let i = 2; i * i <= n; i++) {
+  for (let i = 1; i * i <= n; i++) {
     if (n % i === 0) {
       result.add(i);
       result.add(n / i);
